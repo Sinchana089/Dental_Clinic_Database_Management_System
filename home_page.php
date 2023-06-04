fca115fd9c3e8d88a8dfa11f89458c0d8d379187
Sinchana089 <sinchana.am71@gmail.com>
2023-06-04 20:02:49 +0530

    initial commit

---
+++ home_page.php
@@ -0,0 +1,38 @@
+<html>
+<?php
+session_start();
+?>
+<center>
+
+    <head>
+        <title>
+            DENTAL CLINIC
+        </title>
+    </head>
+
+    <h1>WELCOME TO DENTAL CLINIC HOME PAGE</h1><HR SIZE="3"  COLOR="BLUE">
+    <br>
+
+    <body>
+        <table>
+          <tr><td><img src="den.png" alt="dentist icon"></td></tr> 
+          <tr><td><a href='dentist_login.php'><h3>DENTIST</h3></a></td></tr>
+        </table>
+
+        <table>
+          <td><img src="pat.png" alt="patient icon"></td>
+          <tr><td><a href='patient_login.php'><h3>PATIENT</h3></a></td></tr>
+        </table>
+
+        <table>
+          <td><img src="adm.png" alt="admin icon"></td>
+          <tr><td><a href='adminloginpage.php'><h3>ADMIN</h3></a></td></tr>
+        </table>
+
+
+
+            
+        
+    </body>
+</center>
+</html>
